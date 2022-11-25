e22ed322598a475178acd5014999d3fc02c9c9e2
marcin.kryszewski@onet.eu <marcin.kryszewski@onet.eu>
2022-11-25 06:12:47 +0100

    2022-11-24

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+const App = () => (
+    <div>
+      <Navbar />
+      <div className='app__homePage app__pageDiv'>
+        <div className='home__top'>aaa</div>
+        <div className='home__bottom'>cccc</div>
+      </div>      
+    </div>
+)
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+
+import images from '../../constants/images';
+
+const Navbar = () => (
+  <nav className='app__navbar'>
+    <div className='app_navbar-logo'>
+      <img src={images.logo} alt='LOGO' />
+    </div>
+    <ul className='app__navbar-links'>
+      <li className='p__opensans'><a href='#home'>HOME</a></li>
+      <li className='p__opensans'><a href='#services'>USŁUGI</a></li>
+      <li className='p__opensans'><a href='#contact'>KONTAKT</a></li>
+    </ul>
+    <div className='app__navbar-login'>
+      <a href='#login' className='p__opensans'>Logowanie / Rejestracja</a>
+      <div />
+      <a href="/" className='p__opensans'>Zamów wizytę</a>
+    </div>
+  </nav>
+);
+
+export default Navbar;
