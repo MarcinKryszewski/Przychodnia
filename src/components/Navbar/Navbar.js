f3ed8beddda06c63fce73045ab6f67b79e6b7add
marcin.kryszewski@onet.eu <marcin.kryszewski@onet.eu>
2022-11-27 21:37:15 +0100

    2022-11-27

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdLocalHospital } from 'react-icons/md'
 import './Navbar.css';
 
 import images from '../../constants/images';
 
-const Navbar = () => (
-  <nav className='app__navbar'>
-    <div className='app_navbar-logo'>
-      <img src={images.logo} alt='LOGO' />
-    </div>
-    <ul className='app__navbar-links'>
-      <li className='p__opensans'><Link to='/przychodnia'>HOME</Link></li>
-      <li className='p__opensans'><a href='#home'>O NAS</a></li>
-      <li className='p__opensans'><a href='#services'>USŁUGI</a></li>
-      <li className='p__opensans'><a href='#contact'>KONTAKT</a></li>
-    </ul>
-    <div className='app__navbar-login'>
-      <Link to='/przychodnia/login' className='p__opensans'>Logowanie / Rejestracja</Link>
-      <div />
-      <a href="#order" className='p__opensans'>Zamów wizytę</a>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
+        <img src={images.logo} alt='LOGO' />
+      </div>
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><Link to='/przychodnia'>HOME</Link></li>
+        <li className='p__opensans'><a href='#home'>O NAS</a></li>
+        <li className='p__opensans'><a href='#services'>USŁUGI</a></li>
+        <li className='p__opensans'><a href='#contact'>KONTAKT</a></li>
+      </ul>
+      <div className='app__navbar-login'>
+        <Link to='/przychodnia/login' className='p__opensans'>Logowanie / Rejestracja</Link>
+        <div />
+        <a href="#order" className='p__opensans'>Zamów wizytę</a>
+      </div>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color='fff' fontSize={27} onClick={() => setToggleMenu(true)} />
+        
+        {toggleMenu &&  (
+          <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+            <MdLocalHospital fontSize={27} className='overlay__close' onClick={() => setToggleMenu(false)} />
+            <ul className='app__navbar-smallscreen-links'>
+              <li className='p__opensans'><Link to='/przychodnia'>HOME</Link></li>
+              <li className='p__opensans'><a href='#home'>O NAS</a></li>
+              <li className='p__opensans'><a href='#services'>USŁUGI</a></li>
+              <li className='p__opensans'><a href='#contact'>KONTAKT</a></li>
+            </ul>
+          </div>
+        )}        
+
+      </div>
+    </nav>
+  )
+  
+}
 
 export default Navbar;
